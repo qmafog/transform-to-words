4e15494ada8c57884401e0e96d5999287351ef6e
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-09 13:26:43 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Transform To Words
+
+## Task description
+
+Implement the instance class [Transformer](TransformToWordsTask/Transformer.cs#L8), whose [TransformToWords](TransformToWordsTask/Transformer.cs#L15) instance method converts any real number (`System.Double`) into its `"word format"`. The task definition is given in the XML-comments for the method.   
+_Don't use Framework's converter classes and LINQ._
---
+++ TransformToWords/Transformer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TransformToWords
+{
+    /// <summary>
+    /// Provides transformer methods.
+    /// </summary>
+    public static class Transformer
+    {
+        /// <summary>
+        /// Converts number's digital representation into words.
+        /// </summary>
+        /// <param name="number">Number to convert.</param>
+        /// <returns>Words representation.</returns>
+        public static string TransformToWords(double number)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
